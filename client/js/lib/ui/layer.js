682f4ba7c71fc7126ea9e4b6eb4171a5e4e1f6ee
kampfer <l.w.kampfer@gmail.com>
2012-06-14 16:01:22 +0800

    Update master

--- client/js/lib/ui/layer.js
+++ client/js/lib/ui/layer.js
@@ -12,85 +12,108 @@ kampfer.ui.Layer = kampfer.Class.extend({
 
 		this.isVisible = false;
 		
-		this.opts = kampfer.extend({}, {
-			css : 'kampfer-layer',
-			cssBg : 'kampfer-layer-bg'
-		}, opts);
+		this.isInDocument = false;
+		
+		kampfer.extend(true, this, kampfer.ui.Layer.defaultOptions, opts);
 		
 	},
 	
-	createContainer : function() {
+	getDocument : function() {
+		return kampfer.global.document;	
+	},
+	
+	//创建dom元素
+	createElement : function() {
 		var div = this.element = kampfer.dom.create('div');
 		kampfer.style.setStyle(div, 'display', 'none');
-		kampfer.dom.addClass(div, this.opts.css);
+		kampfer.dom.addClass(div, this.cssName);
 	},
 	
-	destory : function() {
-		var docBody = kampfer.global.document.body;
-		docBody.removeChild(this.bgDiv);
-		docBody.removeChild(this.element);
-		this.bgDiv = null;
-		this.element = null;
+	//将layer从document中移除
+	exitDocument : function() {
+		
+		if(this.element && this.isInDocument) {
+			this.getDocument().body
+				.removeChild(this.element);
+			this.isInDocument = false;
+		}
+		
 	},
 	
-	render : function() {
-		var docBody = kampfer.global.document.body;
-		this.createDiv();
-		this.createBgDiv();
-		this.createBgIframe();
-		docBody.appendChild(this.bgDiv);
-		docBody.appendChild(this.element);
-		this.resizeBg();
-		this.move();
+	//将layer添加到document
+	enterDocument : function() {
+		
+		if(this.isInDocument) {
+			return;
+		}
+		
+		if(!this.element) {
+			this.createElment();
+		}
+		
+		this.getDocument().body
+			.appendChild(this.element);
+		this.isInDocument = true;
+		
 	},
 	
+	//重置layer的大小
+	resize : function(w, h) {
+		w = w + 'px';
+		h = h + 'px';
+		kampfer.style.setStyle(this.element, {
+			width : w,
+			height : h
+		});
+	},
+	
+	//移动layer
+	//TODO 不传递参数，layer将移动到父元素的正中间
 	move : function(x, y) {
-		//fake data
-		//TODO kampfer提供getSize方法
-		var docElem = kampfer.global.document.documentElement,
-			layerSize = {width:300,height:150};
-		
-		if(!x) {
-			x = Math.max(docElem.scrollLeft + docElem.clientWidth / 2 - 
-				layerSize.width / 2, 0);
-		}
-		if(!y) {
-			y = Math.max(docElem.scrollTop + docElem.clientHeight / 2 - 
-				layerSize.height / 2, 0);
-		}
-		
-		this.element.style.left = x + 'px';
-		this.element.style.top = y + 'px';
+		x = x + 'px';
+		y = y + 'px';
+		kampfer.style.setStyle(this.element, {
+			left : x,
+			top : y
+		});
 	},
 	
+	//显示layer
 	show : function() {
 		
 		if( !kampfer.event.trigger(this, 'beforeshow') ) {
 			return;
 		}
 		
-		if(this.element) {
-			this.element.style.display = '';
+		if(!this.isVisible && this.element) {
+			kampfer.style.setStyle(this.element, 'display', '');
+			this.isVisible = true;
 		}
-		this.isVisible = true;
 		
 		kampfer.event.trigger(this, 'show');
 	
 	},
 	
+	//隐藏layer
 	hide : function() {
 		
 		if( !kampfer.event.trigger(this, 'beforehide') ) {
 			return;
 		}
 		
-		if(this.element) {
-			this.element.style.display = 'none';
+		if(this.isVisible && this.element) {
+			kampfer.style.setStyle(this.element, 'display', 'none');
+			this.isVisible = false;
 		}
-		this.isVisible = false;
 		
 		kampfer.event.trigger(this, 'hide');
 		
 	}
 	
 });
+
+kampfer.ui.Layer.defaultOptions = {
+	
+	cssName : 'kampfer-layer'
+	
+};
