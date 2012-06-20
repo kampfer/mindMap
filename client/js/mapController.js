15e112efe04bfcb9f8612a7de0b3d47b33cb4144
kampfer <l.w.kampfer@gmail.com>
2012-06-20 23:36:21 +0800

    update

--- client/js/mapController.js
+++ client/js/mapController.js
@@ -1,4 +1,4 @@
-/*global kampfer*/
+/*global kampfer console*/
 kampfer.require('Class');
 kampfer.require('dom');
 kampfer.require('style');
@@ -8,15 +8,25 @@ kampfer.require('mindMap.Node');
 
 kampfer.provide('mindMap.MapController');
 
+/*
+ * map控制器类
+ * 所有的浏览器事件都在这里被注册。view组件订阅mapController的自定义事件
+ */
 kampfer.mindMap.MapController = kampfer.Class.extend({
 	
 	init : function(currentMapManager) {
 		
 		this.currentMapManager = currentMapManager;
 		
+		//保存一个map视图的引用
 		this.map = null;
 		
+		//保存所有node视图的引用
 		this.nodes = [];
+		
+		this.currentNode = null;
+		
+		this.currentState = this.initialState;
 		
 	},
 	
@@ -41,32 +51,27 @@ kampfer.mindMap.MapController = kampfer.Class.extend({
 	
 	addNode : function(parent) {
 		
-		var newNode = this.currentMapManager.addNode(parent);
-		this.nodes[newNode.id] = new kampfer.mindMap.Node(newNode);
-		
-		newNode.render();
-		
-		kampfer.event.trigger(this, 'addNode');
+		kampfer.event.trigger(this, 'addNode', {
+			parent : parent
+		});
 		
 	},
 	
 	removeNode : function(id) {
 		
-		this.currentMapManager.removeNode(id);
-		this.nodes[id].dispose();
-		delete this.nodes[id];
-		
-		kampfer.event.trigger(this, 'removeNode');
+		kampfer.event.trigger(this, 'removeNode', {
+			id : id
+		});
 		
 	},
 	
 	
 	setNodeText : function(id, text) {
 		
-		this.currentMapManager.setNodeText(id, text);
-		this.nodes[id].setText(text);
-		
-		kampfer.event.trigger(this, 'setNodeText');
+		kampfer.event.trigger(this, 'setNodeText', {
+			id : id,
+			text : text
+		});
 		
 	},
 	
@@ -76,12 +81,164 @@ kampfer.mindMap.MapController = kampfer.Class.extend({
 		
 		function handleEvent(event) {
 			var controller = that;
+			
 			var func = controller.action2Function[controller.currentState][event.type];
-			controller.currentState = func.call(func);
+			controller.currentState = func.call(controller, event);
+		}
+		
+		function handleMenuClick(event) {
+			var controller = that,
+				command;
+				
+			command = controller.getCommand(event);
+			
+			controller.action2Function.editNode(command);
 		}
 		
 		kampfer.addEvent(this.map.element, '*', handleEvent);
+		kampfer.addEvent(this.menu.element, 'click', handleMenuClick);
 		
+	},
+	
+	action2Function : {
+		
+		//鼠标在map上
+		mapActivated : {
+			
+			mouseout : function(event) {
+				var relatedTarget = event.relatedTarget;
+				if( relatedTarget.getAttribute('node-type') === 'node' ) {
+					return 'nodeActivated';
+				}
+				return 'afk';
+			},
+			
+			mousedown : function(event) {
+				if(event.button === 1) {
+					this.saveCursorPosition(event);
+				}else if(event.button === 3) {
+					console.log('button = 3');
+				}
+				return 'mapfocus';
+			}
+
+		},
+		
+		//在map上按住鼠标左键
+		mapfocus : {
+			
+			mousemove : function(event) {
+			
+				var offsetX = event.pageX - this.lastCursorX,
+					offsetY = event.pageY - this.lastCursorY;
+						
+				kampfer.event.trigger(this, 'moveMap', {
+					offsetX: offsetX,
+					offsetY : offsetY
+				});
+				
+				return 'mapfocus';
+				
+			},
+			
+			mouseup : function(event) {
+				return 'mapActivated';
+			}
+			
+		},
+		
+		//鼠标在node上
+		nodeActivated : {
+			
+			mouserout : function() {
+				var relatedTarget = event.relatedTarget;
+				if( relatedTarget.getAttribute('node-type') === 'map' ) {
+					return 'mapActivated';
+				}
+			},
+			
+			mousedown : function(event) {
+				
+				if(event.button === 3) {
+					//显示编辑菜单
+					console.log('显示编辑菜单');
+					return 'nodeActivated';
+				}
+				
+				this.saveCursorPosition(event);
+				this.getCurrentNode(event);
+				
+				return 'nodefocus';
+			}
+			
+		},
+		
+		//在node上按住鼠标左键
+		nodefocus : {
+			
+			mousemove : function() {
+				var offsetX = event.pageX - this.lastCursorX,
+					offsetY = event.pageY - this.lastCursorY;
+						
+				kampfer.event.trigger(this, 'moveNode', {
+					offsetX : offsetX,
+					offsetY : offsetY
+				});
+				
+				return 'nodefocus';
+			},
+			
+			mouseup : function() {
+				return 'nodeActivated';
+			}
+			
+		},
+		
+		//鼠标移出了map
+		afk : {
+			mouseover : function() {
+				return 'mapActivated';
+			}
+		},
+		
+		editNode : {
+			
+		}
+		
+	},
+	
+	initialState : 'mapActivated',
+	
+	saveCursorPosition : function(event) {
+		this.lastCursorX = event.pageX;
+		this.lastCursorY = event.pageY;
+	},
+	
+	getCurrentNode : function(event) {
+		var target = event.target;
+		
+		while( target && target.getAttribute('node-type') !== 'node' ) {
+			target = target.parentNode;
+		}
+		
+		if(target) {
+			this.currentNode = target;
+		}
+	},
+	
+	getCommand : function(event) {
+		var element = event.target,
+			ret;
+			
+		while( element && !(ret = element.getAttribute('action'))) {
+			element = element.parentNode;
+		}
+		
+		if(!ret) {
+			throw 'err';
+		}
+		
+		return ret;
 	}
 	
 });
